1bed8a443b50ca1adfbba32efa14fd7af3c78e86
Aniket1122334 <142924105+Aniket1122334@users.noreply.github.com>
2025-09-01 04:48:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({}));
+app.use(cors({
+  origin: "https://highway-delite-frontend-seven.vercel.app/", // ya frontend ka URL
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
